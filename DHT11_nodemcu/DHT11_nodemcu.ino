fc9809989708928556ee48d8b44efba7b0394187
Dheeren29 <85298596+Dheeren29@users.noreply.github.com>
2024-05-20 19:00:41 +0530

    Add files via upload

---
+++ DHT11_nodemcu/DHT11_nodemcu.ino
@@ -0,0 +1,28 @@
+#include <DHT.h>
+
+#define DHTPIN 2  // DHT11 data pin is connected to GPIO 6 (D6)
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);  // Delay for 2 seconds between readings
+
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor");
+  } else {
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print("%  Temperature: ");
+    Serial.print(temperature);
+    Serial.println("°C");
+  }
+}
